e1e3b87c2458f056e8c2a4b9d362b92f4569bdaf
Rebecca <142409831+extra-jalapenos@users.noreply.github.com>
2025-01-18 15:24:57 +0100

    standard requirements

---
+++ sql/films/create_films.sql
@@ -0,0 +1,15 @@
+-- ## Standard Criteria
+-- Write a SQL CREATE TABLE statement that creates a table to store `films`. The table should have the following columns:
+-- * A unique film id (this should be the primary key, use `SERIAL` or `INTEGER AUTOINCREMENT` to make it increment automatically)
+-- * A unique title
+-- * A genre
+-- * A release year (make this `integer` data type)
+-- * A score out of 10 (make this `integer` data type)
+
+CREATE TABLE films (
+	id SERIAL PRIMARY KEY,
+	title	VARCHAR(255) NOT NULL UNIQUE,
+	genre,
+	release_year INT,
+	score INT
+);
